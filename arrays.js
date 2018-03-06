7a3e3202b5a56fa7c349b5aa868da40e13096fcd
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-06 02:47:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray("hersheys", "butterfingers") {
+  chocolateBars.push()
   return "hersheys", "butterfingers"
 }
